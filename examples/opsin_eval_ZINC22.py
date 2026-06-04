ade0380c298e29c096d6fc8b286f38ae973cba0a
r-fedorov <rostislav.fedorov@h-its.org>
2026-06-04 22:50:52 +0200

    fix: standatrize import mol import

--- examples/opsin_eval_ZINC22.py
+++ examples/opsin_eval_ZINC22.py
@@ -11,7 +11,7 @@ import numpy as np
 import py2opsin
 from datasets import load_dataset
 from tqdm import tqdm
-from utils import standardize_mol
+from bluenamer.utils import standardize_mol
 
 from bluenamer.namer import name_smiles
 
